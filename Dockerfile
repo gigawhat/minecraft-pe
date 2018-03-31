8f0283a07520797f0e8144660b25bc50e8c33af2
Josh Keife <jkeife@gmail.com>
2018-03-31 11:58:11 -0600

    tweaks

--- Dockerfile
+++ Dockerfile
@@ -2,16 +2,13 @@ FROM ubuntu:16.04
 
 # Update, Install Prerequisites
 RUN apt-get -y update && \
-    apt-get install -y sudo wget perl gcc g++ make automake libtool autoconf \
+    apt-get install -y wget perl gcc g++ make automake libtool autoconf \
         m4 gcc-multilib language-pack-en-base software-properties-common \
         python-software-properties && \
     apt-get clean && rm -rf /var/lib/apt/lists/*
 
-# Setup User
-RUN useradd -d /data -s /bin/bash --uid 1000 minecraft
-
 # Stage Files
-COPY server.properties /data/server.properties
+COPY server.properties /server.properties
 COPY start.sh /start.sh
 RUN chmod 0755 /start.sh
 
--- start.sh
+++ start.sh
@@ -2,15 +2,16 @@
 
 set -eux
 
+cp /server.properties /data/server.properties
+
 wget -q -O /data/install.sh https://get.pmmp.io
 chmod 0755 /data/install.sh
 wget -q -O /data/php-linux-x86-64.tar.gz https://jenkins.pmmp.io/job/PHP-7.2-Linux-x86_64/lastSuccessfulBuild/artifact/PHP_Linux-x86_64.tar.gz
-chown -R minecraft:minecraft /data
 
 cd /data
-sudo -E -u minecraft ./install.sh -v development
-sudo -E -u minecraft tar -zxf ./php-linux-x86-64.tar.gz
+./install.sh -r -v development
+tar -zxf ./php-linux-x86-64.tar.gz --no-same-owner
 rm -f /data/install.sh /data/php-linux-x86-64.tar.gz
 
 # start pocketmine
-exec sudo -E -u minecraft ./start.sh
+exec ./start.sh
